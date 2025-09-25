6787fa041717c099a4fbecd808054b3aed08a2ca
herhimstory-source <herhimstory@gmail.com>
2025-09-25 21:13:03 +0900

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 // IMPORTANT: This is a placeholder. The app will prompt you to enter your own URL on first run.
 // You do not need to edit this file directly.
-export const GOOGLE_SHEET_API_URL = 'YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE';
+export const GOOGLE_SHEET_API_URL = 'https://script.google.com/macros/s/AKfycby0VSGbAs8fC7H8HE1k8n63AIe_sAjnsMiDnUBhxn8bBBEvrUeYbyn742LHFgyb1sMIVw/exec';
